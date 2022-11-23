75961571102a8c4fda605514b6f50b18a8305b74
Kante <44691060+kahkwok8@users.noreply.github.com>
2022-11-23 10:06:05 +0800

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,19 @@
+fun f1(var c = 4){
+    println(c)
+}
+
+fun f2(var a = 5){
+
+}
 fun main(){
+    val Pi: Double = 3.14
+    var a: Int
+    a = 2
+    //a = 3
+
     println("hello kolin")
     println("test")
+    println(Pi * a)
+    f1(5)
 }
+
